76808dd539faa24d17858b9fe336afa5c12667fa
parvesh <vparvesh@gmail.com>
2023-05-03 17:23:05 +0530

    improved home page

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSwipeable } from "react-swipeable";
 import HeroVideo from "./HeroVideo";
 import HeroSlogan from "./HeroSlogan";
@@ -6,30 +6,39 @@ import HeroSwipe from "./HeroSwipe";
 import StandardBtnTP from "../../UIs/StandardBtn/StandardBtnTP";
 import styles from "./Hero.module.scss";
 
-export default function Hero({onSwipeUp, className, style}) {
+export default function Hero({ onSwipeUp, className, style }) {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+
   const handlers = useSwipeable(
     {
       onSwipedUp: (eventData) => onSwipeUp(),
     },
     { preventScrollOnSwipe: true }
   );
+
   return (
-    <div className={`${styles['site-hero']} ${styles[className]}`} {...handlers} style={style}>
+    <div
+      className={`${styles["site-hero"]} ${styles[className]}`}
+      {...handlers}
+      style={style}
+    >
       <img
         src={`${process.env.REACT_APP_DOMAIN_NAME}/img/tourista-colored-logo.png`}
         alt="site-logo"
-        className={styles['logo']}
+        className={styles["logo"]}
       />
       <div className={styles["hero-overlay"]} />
-      <HeroVideo />
-      <HeroSlogan />
-      <HeroSwipe />
-      <StandardBtnTP
-        className={styles["hero-controller"]}
-        onClick={() => onSwipeUp()}
-      >
-        Explore Tours
-      </StandardBtnTP>
+      <HeroVideo onVideoLoad={() => setVideoLoaded(true)} />
+      {videoLoaded && <HeroSlogan />}
+      {videoLoaded && <HeroSwipe />}
+      {videoLoaded && (
+        <StandardBtnTP
+          className={styles["hero-controller"]}
+          onClick={() => onSwipeUp()}
+        >
+          Explore Tours
+        </StandardBtnTP>
+      )}
     </div>
   );
 }
--- src/components/hero/HeroVideo.js
+++ src/components/hero/HeroVideo.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
+import Loader from '../../UIs/loader/Loader'
 import styles from "./HeroVideo.module.scss";
 
-export default function HeroVideo() {
+export default function HeroVideo({onVideoLoad}) {
   const [videoLoaded, setVideoLoaded] = useState(false);
+
+  const videoLoadedHandler = ()=>{
+    onVideoLoad()
+    setVideoLoaded(true)
+  }
   return (
     <>
-      {!videoLoaded && <div className={styles["site-hero-background"]}/>}
+      {!videoLoaded && <div className={styles["loading-background"]}><Loader/></div>}
       <video
         className={styles["site-hero-background"]}
         loop
         muted
         autoPlay
-        onLoadedData={() => setVideoLoaded(true)}
+        onLoadedData={videoLoadedHandler}
       >
         <source
           src={`${process.env.REACT_APP_DOMAIN_NAME}/api/v1/tourista-tours-video`}
